40ba1e2eefd260709da80b95f1137f4740ccc88f
Lunjalook <azra.cosickic@gmail.com>
2022-04-06 10:47:10 +0200

    Adding exercise 01 - Objects

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+var myPet = {
+    species: "dog",
+    name: "Bu",
+    legs: 4,
+    friends: "Ax Kuku"
+
+};
+
+
+function myFunction(myObj) {
+    return myObj;
+}
+
+console.log(myFunction(myPet));
+module.exports = { myPet, myFunction };
